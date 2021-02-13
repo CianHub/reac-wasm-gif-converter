9f16824197315ca63b98eda3f71a8822adda8d8b
Cian <23337360+CianHub@users.noreply.github.com>
2021-02-13 20:23:41 +0000

    Add basic styling meta data, improve code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+import { Button, CardMedia } from '@material-ui/core';
 const ffmpeg = createFFmpeg({ log: true });
 
+const getFileType = (file: File) => {
+  if (file) {
+  }
+};
+
 const App: React.FC = () => {
-  const [ready, setReady] = useState(false);
-  const [video, setVideo] = useState<any>();
+  const [ready, setReady] = useState<boolean>(false);
+  const [video, setVideo] = useState<File | null>();
   const [gif, setGif] = useState<string>();
 
   const load = async () => {
@@ -15,32 +21,34 @@ const App: React.FC = () => {
   };
 
   const handleVideoUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.target.files?.item(0) && setVideo(e.target.files?.item(0));
+    e.target.files?.item(0) && setVideo(e.target.files.item(0));
     console.log(e.target.files?.item(0));
   };
 
   const convertToGif = async () => {
-    const type = video.type.split('/')[1];
-    const rootName = `${video.name.split(`.${type}`)}`;
+    if (video) {
+      const type = video.type.split('/')[1];
+      const rootName = `${video.name.split(`.${type}`)}`;
 
-    await ffmpeg.FS('writeFile', `${video.name}`, await fetchFile(video));
-    await ffmpeg.run(
-      '-i',
-      `${video.name}`,
-      '-t',
-      '2.5',
-      '-ss',
-      '2.0',
-      '-f',
-      'gif',
-      `${rootName}.gif`
-    );
+      await ffmpeg.FS('writeFile', `${video.name}`, await fetchFile(video));
+      await ffmpeg.run(
+        '-i',
+        `${video.name}`,
+        '-t',
+        '2.5',
+        '-ss',
+        '2.0',
+        '-f',
+        'gif',
+        `${rootName}.gif`
+      );
 
-    const data = ffmpeg.FS('readFile', `${rootName}.gif`);
-    const url = URL.createObjectURL(
-      new Blob([data.buffer], { type: 'image/gif' })
-    );
-    setGif(url);
+      const data = ffmpeg.FS('readFile', `${rootName}.gif`);
+      const url = URL.createObjectURL(
+        new Blob([data.buffer], { type: 'image/gif' })
+      );
+      setGif(url);
+    }
   };
 
   useEffect(() => {
@@ -48,22 +56,40 @@ const App: React.FC = () => {
   }, []);
 
   return ready ? (
-    <div className="App">
+    <div className="app">
+      <h1 className="heading">Welcome to GIF it!</h1>
+      <h4 className="sub-heading">
+        Please upload an mp4 to convert it to a GIF!
+      </h4>
       {video && (
-        <video controls width="250" src={URL.createObjectURL(video)}></video>
+        <video
+          className="video-preview"
+          controls
+          src={URL.createObjectURL(video)}
+        ></video>
       )}
+      <form className="conversion-form">
+        <Button variant="contained" component="label">
+          Upload File
+          <input
+            type="file"
+            hidden
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              handleVideoUpload(e)
+            }
+          />
+        </Button>
 
-      <input
-        type="file"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          handleVideoUpload(e)
-        }
-      />
+        <h3></h3>
 
-      <h3>Result</h3>
-      <button onClick={convertToGif}>Convert</button>
+        {video && (
+          <Button variant="contained" onClick={convertToGif} color="primary">
+            Convert
+          </Button>
+        )}
 
-      {gif && <img src={gif} width="250px" />}
+        {gif && <img src={gif} width="250px" />}
+      </form>
     </div>
   ) : (
     <p>loading..</p>
